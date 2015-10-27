cfd09d50d51dd1a83ae16e4fe6dd7f50048bf5bb
alkammar <alkammar.mahmoud@gmail.com>
2015-10-27 21:59:52 +0200

    Depricate use builder pattern in use case request/reponse

--- src/lib/morkim/mfw/usecase/UseCaseRequest.java
+++ src/lib/morkim/mfw/usecase/UseCaseRequest.java
@@ -2,14 +2,5 @@ package lib.morkim.mfw.usecase;
 
 
 public class UseCaseRequest {
-
-	public static class Builder {
-
-		public UseCaseRequest build() {
-			return new UseCaseRequest(this);
-		}
-	}
-
-	protected UseCaseRequest(Builder builder) {
-	}
+	
 }
