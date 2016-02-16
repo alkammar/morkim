1fd502f6798c8cea226ae1225da5a64f27a7343c
alkammar <alkammar.mahmoud@gmail.com>
2016-02-16 09:06:51 +0200

    Remove view model in presenter

--- app/src/main/java/lib/morkim/mfw/ui/EmptyPresenter.java
+++ app/src/main/java/lib/morkim/mfw/ui/EmptyPresenter.java
@@ -1,25 +1,9 @@
 package lib.morkim.mfw.ui;
 
-import lib.morkim.mfw.usecase.UseCase;
-import lib.morkim.mfw.usecase.UseCaseResult;
-import lib.morkim.mfw.usecase.UseCaseProgress;
-
 public class EmptyPresenter extends Presenter {
 
 	public EmptyPresenter(Viewable viewable) {
 		super(viewable);
 	}
-
-	@Override
-	public void onUseCaseStart(UseCase useCase) {}
-
-	@Override
-	public void onUseCaseUpdate(UseCaseProgress response) {}
-
-	@Override
-	public void onUseCaseComplete(UseCaseResult response) {}
-
-	@Override
-	public void onUseCaseCancel() {}
 
 }
