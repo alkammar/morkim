418eee3ed7e58ec8761db8de632096e14f8f8b23
alkammar <alkammar.mahmoud@gmail.com>
2016-02-16 09:04:23 +0200

    Remove view model in presenter

--- app/src/main/java/lib/morkim/mfw/ui/Presenter.java
+++ app/src/main/java/lib/morkim/mfw/ui/Presenter.java
@@ -2,17 +2,9 @@ package lib.morkim.mfw.ui;
 
 import android.content.Context;
 
-import java.util.Observable;
-import java.util.Observer;
-
 import lib.morkim.mfw.app.AppContext;
-import lib.morkim.mfw.domain.Model;
-import lib.morkim.mfw.usecase.UseCase;
-import lib.morkim.mfw.usecase.UseCaseProgress;
-import lib.morkim.mfw.usecase.UseCaseResult;
-import lib.morkim.mfw.usecase.UseCaseStateListener;
 
-public abstract class Presenter implements Observer, UseCaseStateListener {
+public abstract class Presenter {
 
 	private AppContext appContext;
 	protected Viewable viewable;
@@ -22,38 +14,6 @@ public abstract class Presenter implements Observer, UseCaseStateListener {
 		this.viewable = viewable;
 		this.appContext = viewable.getMorkimContext();
 	}
-
-	public void unbindViewModel() {
-
-//		viewModel.unregister();
-	}
-
-	public void bindViewModel(Viewable viewable) {
-
-//		viewModel.register(viewable);
-//		viewModel.notifyView();
-	}
-
-	@Override
-	public void update(Observable observable, Object data) {
-		synchronized (this) {
-//			onUpdate(observable, data, viewModel);
-//			if (viewModel.hasListener())
-//				viewModel.notifyUiView();
-		}
-	}
-
-	protected void onUpdate(Observable observable, Object data,
-			ViewModel viewModel) {
-	}
-
-//	public ViewModel getViewModel() {
-//		return viewModel;
-//	}
-//
-//	public void setViewModel(ViewModel viewModel) {
-//		this.viewModel = viewModel;
-//	}
 
 	protected Controller getController() {
 		return viewable.getController();
@@ -70,20 +30,4 @@ public abstract class Presenter implements Observer, UseCaseStateListener {
 	public void setAppContext(AppContext appContext) {
 		this.appContext = appContext;
 	}
-	
-	protected Model getModel() {
-		return appContext.getModel();
-	}
-
-	@Override
-	public void onUseCaseStart(UseCase useCase) {}
-
-	@Override
-	public void onUseCaseUpdate(UseCaseProgress response) {}
-
-	@Override
-	public void onUseCaseComplete(UseCaseResult response) {}
-
-	@Override
-	public void onUseCaseCancel() {}
 }
