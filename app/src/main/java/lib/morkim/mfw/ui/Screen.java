abaa2a7d389013436839ffe5dc477959b818061a
alkammar <alkammar.mahmoud@gmail.com>
2016-02-19 11:10:37 +0200

    Generic MVP

--- app/src/main/java/lib/morkim/mfw/ui/Screen.java
+++ app/src/main/java/lib/morkim/mfw/ui/Screen.java
@@ -1,8 +1,8 @@
 package lib.morkim.mfw.ui;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -12,7 +12,7 @@ import lib.morkim.mfw.R;
 import lib.morkim.mfw.app.AppContext;
 import lib.morkim.mfw.app.MorkimApp;
 
-public abstract class Screen<C extends Controller, P extends Presenter> extends AppCompatActivity implements Viewable<C, P> {
+public abstract class Screen<C extends Controller, P extends Presenter> extends Activity implements Viewable<C, P> {
 
 	public static final String KEY_SCREEN_TRANSITION = "screen.transition";
 
