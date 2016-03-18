7a4dd8e277496b499588830843bce23f0d3bf918
alkammar <alkammar.mahmoud@gmail.com>
2016-03-18 11:20:41 +0200

    Task request builder

--- app/src/main/java/lib/morkim/mfw/usecase/TaskRequest.java
+++ app/src/main/java/lib/morkim/mfw/usecase/TaskRequest.java
@@ -2,5 +2,12 @@ package lib.morkim.mfw.usecase;
 
 
 public class TaskRequest {
-	
+
+	public static abstract class RequestBuilder<Req> implements Builder<Req> {
+
+	}
+
+	protected interface Builder<Req> {
+		Req build();
+	}
 }
