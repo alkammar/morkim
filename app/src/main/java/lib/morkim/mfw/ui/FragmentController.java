3000c1b3ab5a109c75fc5605ec8218766260ee6e
kammar <kammar@.eg.ibm.com>
2016-09-06 08:19:43 +0400

    added fragment controller

---
+++ app/src/main/java/lib/morkim/mfw/ui/FragmentController.java
@@ -0,0 +1,46 @@
+package lib.morkim.mfw.ui;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.view.View;
+
+import lib.morkim.mfw.app.MorkimApp;
+import lib.morkim.mfw.domain.Model;
+
+public abstract class FragmentController<A extends MorkimApp<M, ?>, M extends Model, VA extends UpdateListener>
+		extends Controller<A, M, VA> {
+
+	protected Fragment fragment;
+
+	public FragmentController(A morkimApp) {
+		super(morkimApp);
+	}
+
+	@Override
+	public void attachViewable(Viewable<A, M, VA, ?, ?> viewable) {
+		super.attachViewable(viewable);
+
+		this.fragment = (Fragment) viewable;
+	}
+
+	@Override
+	public View getViewById(int id) {
+		return fragment.getView().findViewById(id);
+	}
+
+	@Override
+	protected void finish() {
+
+	}
+
+	@Override
+	protected A createContext() {
+		//noinspection unchecked
+		return (A) fragment.getActivity().getApplication();
+	}
+
+	@Override
+	public Activity getActivity() {
+		return (Activity) viewable;
+	}
+}
