7beded148d0dc434220ab811d0de2308afd1cfea
kammar <kammar@.eg.ibm.com>
2016-09-22 19:47:11 +0400

    remove parameter from empty presenter constructor

--- app/src/main/java/lib/morkim/mfw/ui/EmptyPresenter.java
+++ app/src/main/java/lib/morkim/mfw/ui/EmptyPresenter.java
@@ -1,10 +1,8 @@
 package lib.morkim.mfw.ui;
 
-import android.content.Context;
-
 public class EmptyPresenter extends Presenter<EmptyController> {
 
-	public EmptyPresenter(Context context) {
+	public EmptyPresenter() {
 		super();
 	}
 
