a76ec2b9b82e9d84b2e9369a13ce7d2d16521067
alkammar <alkammar.mahmoud@gmail.com>
2016-12-01 12:31:35 +0400

    make annotation available to types only

--- annotation/src/main/java/lib/morkim/mfw/mvp/PendingViewableUpdate.java
+++ annotation/src/main/java/lib/morkim/mfw/mvp/PendingViewableUpdate.java
@@ -1,5 +1,10 @@
 package lib.morkim.mfw.mvp;
 
-//@Target(ElementType.TYPE) // on class level
-//@Retention(RetentionPolicy.SOURCE)
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE) // on class level
+@Retention(RetentionPolicy.SOURCE)
 public @interface PendingViewableUpdate {}
